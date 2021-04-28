3b28d8bf8875170874f6d7298be97fa7e261da5a
AL1645 <barry.dam@alliander.com>
2021-04-28 08:53:11 +0000

    1.0.2 incl readme

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
 # Alliander BTP-CF-DESTINATION
-Readme volgt snel!
+This package can be used to make a connection to the connectivity service of the SAP Business Technology Platform (BTP / SCP).
+The destination service is being used to retrieve the connectivity data. The @sap-cloud-sdk/core package (which uses Axios) is being used as http client.
+
+You can used this package as a express middleware proxy or as a standalone function, see the examples below.
+It can be used for all types of ProxyTypes incl OnPremise, which makes the request based on your user JWT.
+
+## Prerequisites
+- destination and destination instance created
+- connectivity instance created
+- xsuaa instance created
+- all of the above instances bound to the node app, e.g. via manifest.yml:
+```yaml
+applications:
+- name: my_app
+  path: my_app
+  memory: 128M
+  services:
+    - xsuaa-instance
+    - connectivity-instance
+    - destination-instance
+```
+
+## Usage examples
+First in the SAP BTP Cockpit we have created a destination with the following settings:
+```
+name: my-dest
+type: HTTP
+url: http://my-destination-url:443
+ProxyType: Internet
+AuthentiCation: BasicAuthentication
+```
+(If you need to send along the logged used you need to configure your destination configuration in the SAP BTP Cockpit to use ProxyType: OnPremise and Authentication: PrincipalPropagation up your destination.)
+
+### Create a module
+The best practice is to first create a module in which you define the destination, for example place it in a /modules/mydestination/index.js file
+```js
+const BtpCfDestination = require("@alliander/btp-cf-destination");
+module.exports = new BtpCfDestination(
+	"my-dest", // the destination name defined in the SAP BTP Cockpit destination configuration instance
+	{ // optional params
+		path : '/path/to/services.xsodata' 
+	});
+```
+
+### use as express middleware
+
+```js
+const myDest = require("/modules/mydestination");
+
+//Proxy the request directely to the service
+router.use("/odata", myDest.expressMiddleware());
+
+// or specify a path
+router.use("/odata/example", myDest.expressMiddleware({ path: "/specific-endpoint" }));
+```
+
+### Fire requests manually
+
+```js
+const myDest = require("/modules/mydestination");	
+
+router.post("/send-example", async (req, res) => {
+	const result = await myDest.request(
+		"/books",
+		{ // optional params
+			method 			: "POST",
+			data 			: {
+				name	: "Moby Dick",
+				author	: "Herman Melville"
+			}
+		}
+	);
+	res.status(result.status).set(result.headers).send(result.data);
+});
+
+// In case you need to send along the user JWT (needed when the proxytype is OnPremise and Authentication is PrincipalPropagation)
+router.post("/send-example-with-user-credentials", async (req, res) => {
+	const result = await myDest.request("/books", { userJwt: myDest.getUserJwtToken(req) });
+	res.status(result.status).set(result.headers).send(result.data);
+});
+
+```
+request additional optional params:
+- method	(default: "get");
+- responseType (default: "json")
+- headers (default {})
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 'use strict';
-const { getDestination, executeHttpRequest} = require('@sap-cloud-sdk/core'),
+const { getDestination, executeHttpRequest } = require('@sap-cloud-sdk/core'),
 	expressWrap = fn => (...args) => fn(...args).catch(args[2]);
 
 class BtpCfDestination {
-	
+
 	/**
 	 * Constructor
 	 * @param {string} destination - destination name corresponding to the destination created in the SAP BTP Cockpit
@@ -11,12 +11,12 @@ class BtpCfDestination {
 	 */
 	constructor(destination, { path = "/" } = {}) {
 		if (!destination) { throw new Error("Destination is required"); }
-		if (typeof destination !== "string") { 
+		if (typeof destination !== "string") {
 			console.log(destination);
-			throw new Error(`Destination param is a ${typeof destination } but should be a string`);
+			throw new Error(`Destination param is a ${typeof destination} but should be a string`);
 		}
-		this._destination = destination; 
-		this._path = !path.startsWith("/") ? `/${path}` : path ;
+		this._destination = destination;
+		this._path = !path.startsWith("/") ? `/${path}` : path;
 	}
 
 	/**
@@ -32,7 +32,7 @@ class BtpCfDestination {
 			return oReq.attr.getAppToken();
 		}
 	}
-	
+
 	/**
 	 * @param {string} url 
 	 * @param {object} mParams
@@ -44,90 +44,90 @@ class BtpCfDestination {
 	 * @returns {Axios result} 	{ data, headers, status}
 	 */
 	async request(url, { // optional options
-		method 			= "GET",
-		responseType 	= "json",
-		headers 		= {},
-		data 			= null,
-		userJwt 		= null
+		method = "GET",
+		responseType = "json",
+		headers = {},
+		data = null,
+		userJwt = null
 	} = {}) {
-		const oDestination = await getDestination(this._destination, { 
-			userJwt, 
-			enableCircuitBreaker : false // prevents memory leaks
+		const oDestination = await getDestination(this._destination, {
+			userJwt,
+			enableCircuitBreaker: false // prevents memory leaks
 		});
 		oDestination.url += `${this._path}${url.startsWith("/") ? url : `/${url}`}`;
 		// Proxy (needed for the SAP Business Application Studio)
 		if ("HTTP_PROXY" in process.env && oDestination.proxyType === "OnPremise") {
 			const aProxy = process.env.HTTP_PROXY.split(":");
-			oDestination.proxyConfiguration.port = aProxy[2]; 
+			oDestination.proxyConfiguration.port = aProxy[2];
 			oDestination.proxyConfiguration.host = aProxy[1].replace("//", "");
 		}
 		return executeHttpRequest(oDestination, { headers, method, data, responseType });
 	}
-	
+
 	async create(sPath, mData) {
 		if (!sPath) { throw "Path is not defined"; }
 		return this.request(sPath, {
-			method	: "POST",
-			data	: mData
+			method: "POST",
+			data: mData
 		});
 	}
-	
+
 	async read(sPath) {
 		if (!sPath) { throw "Path is not defined"; }
 		return this.request(sPath, {
-			method	: "GET"
+			method: "GET"
 		});
 	}
-	
+
 	async update(sPath, mData) {
 		if (!sPath) { throw "Path is not defined"; }
 		return this.request(sPath, {
-			method	: "PUT",
-			data	: mData
+			method: "PUT",
+			data: mData
 		});
 	}
-	
+
 	async delete(sPath) {
 		if (!sPath) { throw "Path is not defined"; }
 		return this.request(sPath, {
-			method	: "DELETE"
+			method: "DELETE"
 		});
 	}
-	
+
 	/**
 	 * @param {object} [mParams]
 	 * 	@param {string} [mParams.path] 		- Url path set as base
 	 *  @param {string} [mParams.headers] 	- headers that needs to be send along with te request
 	 */
-	expressMiddleware({ path = "", headers = {}} = {}) {
+	expressMiddleware({ path = "", headers = {} } = {}) {
 		if (path && !path.endsWith("/")) { path += "/"; }
 		return expressWrap(async (oReq, oRes) => {
 			let sReqBaseurl = oReq.baseUrl;
-			if (oReq.originalUrl.endsWith("/") && !sReqBaseurl.endsWith("/")) { sReqBaseurl += "/";	}
+			if (oReq.originalUrl.endsWith("/") && !sReqBaseurl.endsWith("/")) { sReqBaseurl += "/"; }
 			this.expressRequestHandler(oReq, oRes, {
-				url		: oReq.originalUrl.replace(sReqBaseurl, path).replace(/\/\//g, "/"),
-				method	: oReq.method,
-				data	: oReq.body || null,
+				url: oReq.originalUrl.replace(sReqBaseurl, path).replace(/\/\//g, "/"),
+				method: oReq.method,
+				data: oReq.body || null,
 				headers
 			});
 		});
 	}
 
-	async expressRequestHandler(oReq, oRes, { url, method = "GET" , headers = {}, data = null } = {})  {
+	async expressRequestHandler(oReq, oRes, { url, method = "GET", headers = {}, data = null } = {}) {
 		try {
-			const mResult = await this.request(url, { 
-				method, headers, data, 
-				userJwt 		: BtpCfDestination.getUserJwtToken(oReq), 
-				responseType 	: 'stream' // stream needed for possible file downloading
-			}); 
+			const mResult = await this.request(url, {
+				method, headers, data,
+				userJwt: BtpCfDestination.getUserJwtToken(oReq),
+				responseType: 'stream' // stream needed for possible file downloading
+			});
 			oRes.writeHead(mResult.status, mResult.headers);
 			mResult.data.pipe(oRes);
 		} catch (e) {
 			oRes.status(e.status || 500).json({
-				message	 : e.message,
-				stack	 : e.stack
+				message: e.message,
+				stack: e.stack
 			});
-		}							
+		}
 	}
 }
 module.exports = BtpCfDestination;
